2c877298ba08cacce0313190809a744658a3276d
raviyadav2509 <ravs_avs@yahoo.com>
2026-09-19 22:08:11 +1000

    Initialize BAS history database on startup

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ from flask import Flask
 from config import FLASK_SECRET_KEY
 from routes.bas_routes import bas_bp
 from routes.qbo_routes import qbo_bp
+from services.database import init_db
 
 
 def create_app():
     app = Flask(__name__)
     app.secret_key = FLASK_SECRET_KEY
+    init_db()
 
     app.register_blueprint(qbo_bp)
     app.register_blueprint(bas_bp)
