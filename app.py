329b3a5e1b23c3ed15d90037346125ed9bd507db
raviyadav2509 <ravs_avs@yahoo.com>
2026-09-20 11:59:25 +1000

    Register authentication blueprint

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 from config import FLASK_SECRET_KEY
+from routes.auth_routes import auth_bp
 from routes.bas_routes import bas_bp
 from routes.qbo_routes import qbo_bp
 from services.database import init_db
@@ -11,6 +12,7 @@ def create_app():
     app.secret_key = FLASK_SECRET_KEY
     init_db()
 
+    app.register_blueprint(auth_bp)
     app.register_blueprint(qbo_bp)
     app.register_blueprint(bas_bp)
 
