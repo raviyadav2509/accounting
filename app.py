833736b01c3f404e4ef09b8aa2990f86e6e31321
raviyadav2509 <ravs_avs@yahoo.com>
2026-09-20 12:06:52 +1000

    Register client management blueprint

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from config import FLASK_SECRET_KEY
 from routes.auth_routes import auth_bp
 from routes.bas_routes import bas_bp
+from routes.client_routes import clients_bp
 from routes.qbo_routes import qbo_bp
 from services.database import init_db
 
@@ -17,6 +18,7 @@ def create_app():
     init_db()
 
     app.register_blueprint(auth_bp)
+    app.register_blueprint(clients_bp)
     app.register_blueprint(qbo_bp)
     app.register_blueprint(bas_bp)
 
